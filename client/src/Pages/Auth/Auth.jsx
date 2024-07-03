a6ca4a93b416ad2640443da58106129233b8751f
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-07-03 17:27:52 +0530

    basic login form and post hook

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,8 +1,35 @@
+import { useState } from 'react';
 import './Auth.scss';
+import usePostFetch from '../../hooks/usePostFetch';
 
 const Auth = () => {
+    const [userCredentials, setUserCredentials] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        setUserCredentials(prev => {
+            return {
+                ...prev,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+
+    const handleSubmit = async() => {
+        const data = await usePostFetch('/login', userCredentials);
+        console.log(data);
+    }
+
     return (
-        <div className="auth">Auth</div>
+        <div className="auth">
+            <input type="text" name='username' value={userCredentials.username} onChange={(e) => handleChange(e)} />
+            <input type="email" name='email' value={userCredentials.email} onChange={(e) => handleChange(e)} />
+            <input type="password" name='password' value={userCredentials.password} onChange={(e) => handleChange(e)} />
+            <button onClick={handleSubmit}>Login</button>
+        </div>
     )
 }
 
---
+++ client/src/hooks/usePostFetch.js
@@ -0,0 +1,26 @@
+import { makeRequest } from "./makeRequest";
+
+const usePostFetch = async (url, bodyData) => {
+    try {
+        const resp = await makeRequest.post(url, { ...bodyData }, {
+            withCredentials: true
+        })
+        // console.log(resp)
+        if (resp.data) {
+            return { data: resp.data };
+        }
+        else {
+            return { error: resp.error };
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return {
+            success: false,
+            status: err.response.status,
+            error: `${err.response.data.error}`
+        }
+    }
+}
+
+export default usePostFetch;
