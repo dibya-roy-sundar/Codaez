c121c62b95446222699cbd65a2bff52d8ed5d660
Yash Mittal <mittalyas1234@gmail.com>
2024-07-05 16:00:10 +0530

    changes done told by vanshul

--- client/src/components/Register/CompleteProfile.jsx
+++ client/src/components/Register/CompleteProfile.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 import './CompleteProfile.scss';
-import { FaGoogle } from 'react-icons/fa';
-import OTPVerification from './OtpVerification';
+// import { FaGoogle } from 'react-icons/fa';
+// import OTPVerification from './OtpVerification';
+import { useSelector } from 'react-redux';
+import { FaCheck } from 'react-icons/fa';
 
 const CompleteProfile = () => {
     const [currentStep, setCurrentStep] = useState(1);
     const [profilePicture, setProfilePicture] = useState(null);
-    const [otpOpen, setOtpOpen] = useState(false);
-
+    const [formValues, setFormValues] = useState({
+        firstName: '',
+        lastName: '',
+        college: '',
+        leetcode: '',
+        codeforces: '',
+        codechef: ''
+    });
+    // const [otpOpen, setOtpOpen] = useState(false);
+    const user = useSelector(state => state.auth.auth);
+    console.log('user', user);
     const handleNextStep = () => {
-        if (currentStep === 1) {
-            setOtpOpen(true);
-        } else {
-            setCurrentStep((prev) => prev + 1);
-        }
+        // data saving code
+
+        setCurrentStep((prev) => prev + 1);
     };
 
     const handlePrevStep = () => {
@@ -35,25 +44,41 @@ const CompleteProfile = () => {
             reader.readAsDataURL(file);
         }
     };
-    const closeOtpModal = () => {
-        setOtpOpen(false);
-        setCurrentStep(2);
+    const handleStepChange = (step) => {
+        setCurrentStep(step);
+    }
+    // const closeOtpModal = () => {
+    //     setOtpOpen(false);
+    //     setCurrentStep(2);
+    // };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues((prev) => ({
+            ...prev,
+            [name]: value
+        }));
     };
 
     return (
         <div className="register-container">
             <div className="register-content">
                 <div className="progress-bar">
-                    <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-                        <div className={`progress-step ${currentStep >= 1 ? 'active' : ''}`}>1</div>
+                    <div style={{ display: "flex", alignItems: "center", gap: "1rem", cursor: 'pointer' }}>
+                        <div className={`progress-step ${currentStep >= 1 ? 'active' : ''}`} onClick={() => handleStepChange(1)}>
+                            {currentStep > 1 ? <FaCheck /> : '1'}
+                        </div>
                         <p>User Credentails</p>
                     </div>
-                    <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-                        <div className={`progress-step ${currentStep >= 2 ? 'active' : ''}`}>2</div>
+                    <div style={{ display: "flex", alignItems: "center", gap: "1rem", cursor: 'pointer' }}>
+                        <div className={`progress-step ${currentStep >= 2 ? 'active' : ''}`} onClick={() => handleStepChange(2)}>
+                            {currentStep > 2 ? <FaCheck /> : '2'}
+                        </div>
                         <p>User Details</p>
                     </div>
-                    <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-                        <div className={`progress-step ${currentStep >= 3 ? 'active' : ''}`}>3</div>
+                    <div style={{ display: "flex", alignItems: "center", gap: "1rem", cursor: 'pointer' }}>
+                        <div className={`progress-step ${currentStep >= 3 ? 'active' : ''}`} onClick={() => handleStepChange(3)}>
+                            {currentStep > 3 ? <FaCheck /> : '3'}
+                        </div>
                         <p>Coding Usenames</p>
                     </div>
                 </div>
@@ -63,27 +88,33 @@ const CompleteProfile = () => {
                             <div className="register-step">
                                 <h3>User Credentails</h3>
                                 <div className="input-wrap">
-                                    <input type="text" id="username" name="username" required />
-                                    <label htmlFor="username">Username</label>
+                                    <input type="text" id="username" name="username" value={user.username} readOnly required />
+
                                 </div>
                                 <div className="input-wrap">
-                                    <input type="email" id="email" name="email" required />
-                                    <label htmlFor="email">Email</label>
+                                    <input type="email" id="email" name="email" value={user.email} readOnly required />
+
                                 </div>
-                                <div className="input-wrap">
+                                {/* <div className="input-wrap">
                                     <input type="password" id="password" name="password" required />
                                     <label htmlFor="password">Password</label>
-                                </div>
-                                <button type="button" className="register-button" onClick={handleNextStep}>
+                                </div> */}
+                                {/* <button type="button" className="register-button" onClick={handleNextStep}>
                                     Register
-                                </button>
-                                <div className="google-button">
+                                </button> */}
+                                {/* <div className="google-button">
                                     <div className="google-btn-image">
                                         <FaGoogle />
                                     </div>
                                     <div>
                                         <span>Register with Google</span>
                                     </div>
+                                </div> */}
+                                <div className="register-buttons">
+                                    <button type="button" className="register-button" onClick={handleNextStep}>
+                                        Next
+                                    </button>
+
                                 </div>
                             </div>
                         )}
@@ -100,25 +131,26 @@ const CompleteProfile = () => {
                                     </div>
                                     <div className="name-fields">
                                         <div className="input-wrap">
-                                            <input type="text" id="firstName" name="firstName" required />
+                                            <input type="text" id="firstName" name="firstName" value={formValues.firstName} onChange={handleInputChange} required />
                                             <label htmlFor="firstName">First Name</label>
                                         </div>
                                         <div className="input-wrap">
-                                            <input type="text" id="lastName" name="lastName" required />
+                                            <input type="text" id="lastName" name="lastName" value={formValues.lastName} onChange={handleInputChange} required />
                                             <label htmlFor="lastName">Last Name</label>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="input-wrap">
-                                    <input type="text" id="college" name="college" required />
+                                    <input type="text" id="college" name="college" value={formValues.college} onChange={handleInputChange} required />
                                     <label htmlFor="college">College Name</label>
                                 </div>
                                 <div className="register-buttons">
-                                    <button type="button" className="register-button" onClick={handleNextStep}>
-                                        Next
-                                    </button>
+
                                     <button type="button" className="register-button" onClick={handlePrevStep}>
                                         Back
+                                    </button>
+                                    <button type="button" className="register-button" onClick={handleNextStep}>
+                                        Next
                                     </button>
                                 </div>
                             </div>
@@ -127,23 +159,23 @@ const CompleteProfile = () => {
                             <div className="register-step">
                                 <h3>Coding Usernames</h3>
                                 <div className="input-wrap">
-                                    <input type="text" id="leetcode" name="leetcode" required />
+                                    <input type="text" id="leetcode" name="leetcode" value={formValues.leetcode} onChange={handleInputChange} required />
                                     <label htmlFor="leetcode">LeetCode Username</label>
                                 </div>
                                 <div className="input-wrap">
-                                    <input type="text" id="codeforces" name="codeforces" required />
+                                    <input type="text" id="codeforces" name="codeforces" value={formValues.codeforces} onChange={handleInputChange} required />
                                     <label htmlFor="codeforces">Codeforces Username</label>
                                 </div>
                                 <div className="input-wrap">
-                                    <input type="text" id="codechef" name="codechef" required />
+                                    <input type="text" id="codechef" name="codechef" value={formValues.codechef} onChange={handleInputChange} required />
                                     <label htmlFor="codechef">CodeChef Username</label>
                                 </div>
                                 <div className='register-buttons'>
-                                    <button type="submit" className="register-button">
-                                        Submit
-                                    </button>
                                     <button type="button" className="register-button" onClick={handlePrevStep}>
                                         Back
+                                    </button>
+                                    <button type="submit" className="register-button">
+                                        Submit
                                     </button>
                                 </div>
                             </div>
@@ -151,7 +183,7 @@ const CompleteProfile = () => {
                     </form>
                 </div>
             </div>
-            <OTPVerification isOpen={otpOpen} onClose={closeOtpModal} />
+            {/* <OTPVerification isOpen={otpOpen} onClose={closeOtpModal} /> */}
         </div>
     );
 };
