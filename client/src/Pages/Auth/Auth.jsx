42805a4e16fb541384cc26f79d3dcd134e8e4f5f
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-07-05 14:04:32 +0530

    Register integrated

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { setAuth } from '../../redux/authReducer';
 import { useNavigate } from 'react-router-dom';
 import Login from '../../components/Login/Login';
+import Register from '../../components/Register/Register';
 
 const Auth = () => {
     const dispatch = useDispatch();
@@ -49,9 +50,49 @@ const Auth = () => {
         }
     }
 
+    const [registerUserCredentials, setRegisterUserCredentials] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const handleRegisterChange = (e) => {
+        setRegisterUserCredentials(prev => (
+            {
+                ...prev,
+                [e.target.name]: e.target.value
+            }
+        ));
+    }
+
+    const handleRegisterSubmit = async (e) => {
+        e.preventDefault();
+        const data = await usePostFetch('/register',registerUserCredentials);
+        // console.log(data.data);
+
+        if (data.data && data.data.user) {
+            // toast.success(`Hello ${data.data.user.name}`, {
+            //     position: toast.POSITION.TOP_LEFT
+            // });
+            dispatch(setAuth(data.data.user));
+            navigate('/completeprofile');
+        }
+        else if (data.data) {
+            // toast.warn(data.data.error || data.data.message, {
+            //     position: toast.POSITION.TOP_LEFT
+            // });
+        }
+        else {
+            // toast.error(data.error, {
+            //     position: toast.POSITION.TOP_LEFT
+            // });
+        }
+    }
+
     return (
         <div className="auth">
             <Login loginUserCredentials={loginUserCredentials} handleLoginChange={handleLoginChange} handleLoginSubmit={handleLoginSubmit} />
+            <Register registerUserCredentials={registerUserCredentials} handleRegisterChange={handleRegisterChange} handleRegisterSubmit={handleRegisterSubmit} />
         </div>
     )
 }
