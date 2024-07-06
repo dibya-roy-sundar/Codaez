e682b8add5b5191912884bd6554c176764aeb0d0
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-07-06 16:08:03 +0530

    bug fix

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,19 +11,19 @@ const upload = multer({ storage })
 const router = express.Router({ mergeParams: true });
 
 router.route('/login').post(catchAsync(login));
-router.route('/register').post(upload.single('avatar'), catchAsync(register));
-router.route('/complete-profile').post(catchAsync(isLoggedIn),upload.single('avatar'), catchAsync(completeProfile));
+router.route('/register').post(catchAsync(register));
+router.route('/complete-profile').put(catchAsync(isLoggedIn),upload.single('avatar'), catchAsync(completeProfile));
 router.route('/logout').get(catchAsync(logout));
 router.route('/changepw')
     // .get(changePassword)//changepasswordform
-    .post(catchAsync(isLoggedIn), catchAsync(changePassword));
+    .put(catchAsync(isLoggedIn), catchAsync(changePassword));
 router.route('/setusername').post(catchAsync(isLoggedIn), catchAsync(setUsername));
 router.route('/userdetails').get( catchAsync(userDetails));//search
 router.route('/sendfrequest').post(catchAsync(isLoggedIn), catchAsync(sendFollowRequest));
 router.route('/acceptfrequest').post(catchAsync(isLoggedIn), catchAsync(acceptFollowRequest));
 router.route('/rejectfrequest').post(catchAsync(isLoggedIn), catchAsync(rejectFollowRequest));
-router.route('/update-profile').post(catchAsync(isLoggedIn), catchAsync(updateProfile))
-router.route('/update-avatar').post(catchAsync(isLoggedIn), upload.single('avatar'), catchAsync(editAvatar))
+router.route('/update-profile').put(catchAsync(isLoggedIn), catchAsync(updateProfile))
+router.route('/update-avatar').put(catchAsync(isLoggedIn), upload.single('avatar'), catchAsync(editAvatar))
 router.route('/profile/:username').get(catchAsync(profile));
 
 module.exports = router;
