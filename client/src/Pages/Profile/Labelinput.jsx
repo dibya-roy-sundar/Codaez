3b088877729fb62f8905df52945625014080f8ef
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-07-06 19:06:42 +0530

    removed fontawesome

--- client/src/Pages/Profile/Labelinput.jsx
+++ client/src/Pages/Profile/Labelinput.jsx
@@ -1,32 +1,30 @@
 /* eslint-disable react/prop-types */
-import {  useState } from "react"
+import { useState } from "react"
 import "./Labelinput.scss"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
+import { FaEye, FaEyeSlash } from 'react-icons/fa6';
 
 // eslint-disable-next-line react/prop-types
-const Labelinput = ({name,label,value,edit,password,icon,image,type,onChange,...props}) => {
-  const [showpw,setShowpw]=useState(false)
- 
-   
+const Labelinput = ({ name, label, value, edit, password, icon, image, type, onChange, ...props }) => {
+    const [showpw, setShowpw] = useState(false)
 
-  return (
-    <>
-        <div className="container">
-            <div className="icon">
-                {icon && icon}
-                {image && <img src={image}  alt=" platform icon" />}
-                
-             <label htmlFor={name}>{label? label : name}</label>
+
+
+    return (
+        <>
+            <div className="container">
+                <div className="icon">
+                    {icon && icon}
+                    {image && <img src={image} alt=" platform icon" />}
+
+                    <label htmlFor={name}>{label ? label : name}</label>
+                </div>
+                {(edit && !props?.disabled) ? <input {...props} type={password ? (showpw ? "text" : "password") : (type ? type : "text")} name={name} id={name} value={value} onChange={(e) => {
+                    onChange(name, e.target.value);
+                }} /> : <p>{value?.length > 0 ? value : "- - -"}</p>}
+                {edit && password && (showpw ? <FaEyeSlash onClick={() => { setShowpw((prev) => !prev) }} className="eye" /> : <FaEye onClick={() => { setShowpw((prev) => !prev) }} className="eye" />)}
             </div>
-            {(edit && !props?.disabled ) ?<input {...props} type={password ? (showpw?"text":"password") : (type?type:"text")} name={name} id={name} value={value} onChange={(e)=>{
-                onChange(name,e.target.value);
-            }}  />: <p>{value?.length>0 ? value :"- - -"}</p>}
-           {edit && password && <FontAwesomeIcon onClick={()=>{setShowpw((prev)=>!prev)}} className="eye" icon={showpw?faEye:faEyeSlash} />}
-            
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Labelinput
