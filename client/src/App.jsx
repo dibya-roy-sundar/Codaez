9ea8b3b36471413a5081211048f2edccadadb21d
Dibya Sunadr Roy <dibya.roy.12345@gmail.com>
2024-07-18 01:20:35 +0530

    add 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,69 +1,105 @@
-import './App.css'
-import Auth from './Pages/Auth/Auth';
+import "./App.css";
+import Auth from "./Pages/Auth/Auth";
 import Dashboard from "./Pages/Dashboard/Dashboard";
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
-import Landing from './Pages/Landing/Landing';
-import Leaderboard from './Pages/Leaderboard/Leaderboard';
-import Profile from './Pages/Profile/Profile';
-import Navbar from './Pages/Navbar/Navbar'
-import Sidebar from './components/Sidebar/Sidebar';
-import CompleteProfile from './components/Register/CompleteProfile';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Landing from "./Pages/Landing/Landing";
+import Leaderboard from "./Pages/Leaderboard/Leaderboard";
+import Profile from "./Pages/Profile/Profile";
+import Navbar from "./Pages/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import CompleteProfile from "./components/Register/CompleteProfile";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from "react-toastify";
+import Error_404 from "./Pages/404Page/Error_404";
 
 const Layout = () => {
-    return (
-        <div className='app'>
-            <ToastContainer autoClose={3000} draggablePercent={50} limit={3} theme='dark' stacked />
-            {/* <Navbar /> */}
-            {/* <Sidebar /> */}
-            <Outlet />
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <ToastContainer
+        autoClose={3000}
+        draggablePercent={50}
+        limit={3}
+        theme="dark"
+        stacked
+      />
+      {/* <Navbar /> */}
+      {/* <Sidebar /> */}
+      <Outlet />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
         path: "/",
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: <Landing />
-            },
-            {
-                path: "/dashboard",
-                element: <><Navbar /><Sidebar /><Dashboard /></>
-            },
-            {
-                path: "/leaderboard",
-                element: <><Navbar /><Sidebar /><Leaderboard /></>
-            },
-            {
-                path: "/auth",
-                element: <Auth />
-            },
-            {
-                path: "/profile/:username",
-                element: <><Navbar /><Sidebar /><Profile /></>
-            },
-            {
-                path: "/completeprofile",
-                element: <CompleteProfile />
-            },
-        ]
-    },
-])
+        element: <Landing />,
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <>
+            <Navbar />
+            <Sidebar />
+            <Dashboard />
+          </>
+        ),
+      },
+      {
+        path: "/leaderboard",
+        element: (
+          <>
+            <Navbar />
+            <Sidebar />
+            <Leaderboard />
+          </>
+        ),
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/profile/:username",
+        element: (
+          <>
+            <Navbar />
+            <Sidebar />
+            <Profile />
+          </>
+        ),
+      },
+      {
+        path: "/completeprofile",
+        element: <CompleteProfile />,
+      },
+      { path: "404", element:(
+      <>
+      <Navbar />
+      <Sidebar />
+      <Error_404 />
+      </>  )},
+      { path: "*", element: <Navigate to="/404" replace /> },
+    ],
+  },
+]);
 
 function App() {
-
-    return (
-        <>
-            <div>
-                <RouterProvider router={router} />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
