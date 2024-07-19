612738f402fbe69f37a89d48188eb39363fd9c9f
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-07-20 04:31:42 +0530

    compprofile, profile responsive

--- client/src/components/Register/CompleteProfile.jsx
+++ client/src/components/Register/CompleteProfile.jsx
@@ -14,19 +14,20 @@ import { toast } from 'react-toastify';
 import { ClipLoader } from "react-spinners";
 import { SiTicktick } from "react-icons/si";
 import { RiErrorWarningLine } from "react-icons/ri";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 
 const CompleteProfile = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    
-    const avatarref=useRef();
+
+    const avatarref = useRef();
 
     const [searchParams, setSearchParams] = useSearchParams();
     const [InputValue, setInputValue] = useState("");
     const [loading, setLoading] = useState(null);
     const [unique, setUnique] = useState(null);
-    const [isnormal,setisnormal]=useState(false);
+    const [isnormal, setisnormal] = useState(false);
 
     const userFromRedux = useSelector(state => state.auth.auth)
     const [currentStep, setCurrentStep] = useState(1);
@@ -44,12 +45,9 @@ const CompleteProfile = () => {
 
 
     const [user, setUser] = useState();
-    const [isFormSubmitting,setisFormSubmitting]=useState(false);
+    const [isFormSubmitting, setisFormSubmitting] = useState(false);
 
 
-
-
-    
     useEffect(() => {
         if (searchParams.get('email')) {
             setUser({
@@ -58,7 +56,9 @@ const CompleteProfile = () => {
                 avatarUrl: searchParams.get('avatarUrl'),
             });
             setProfilePicture(searchParams.get('avatarUrl'));
-            setFormValues(prev => ({ ...prev, firstName: searchParams.get('name') }));
+            const name = searchParams.get('name')?.split(' ');
+            const lastname = name?.slice(1,)?.join(' ');
+            setFormValues(prev => ({ ...prev, firstName: name[0], lastName: lastname }));
         }
         else {
             setUser(userFromRedux);
@@ -67,74 +67,74 @@ const CompleteProfile = () => {
 
     useEffect(() => {
         const changeuname = async () => {
-          const { data } = await usePutHook("/change-username", {
-            username: InputValue,
-            save: false,
-          });
-    
-          if (data) {
-            setLoading(false);
-            if (data.success) {
-              setUnique(true);
-              // unique username found
-              if(data.isnormal){
-                setisnormal(true);
-              }else{
-                setisnormal(false);
-                toast.warn(" username must be 18 characters or fewer", {
-                  position: "top-right"
-                });
-              }
-            } else {
-              setUnique(false);
-              toast.warn(data.data.error || data.data.message, {
-                 position: "top-right"
-              });
-              // username already taken - Toastify
+            const { data } = await usePutHook("/change-username", {
+                username: InputValue,
+                save: false,
+            });
+
+            if (data) {
+                setLoading(false);
+                if (data.success) {
+                    setUnique(true);
+                    // unique username found
+                    if (data.isnormal) {
+                        setisnormal(true);
+                    } else {
+                        setisnormal(false);
+                        toast.warn(" username must be 18 characters or fewer", {
+                            position: "top-right"
+                        });
+                    }
+                } else {
+                    setUnique(false);
+                    toast.warn(data.data.error || data.data.message, {
+                        position: "top-right"
+                    });
+                    // username already taken - Toastify
+                }
             }
-          }
         };
-    
+
         if (InputValue.length > 0) {
-          changeuname();
+            changeuname();
         }
-      }, [InputValue]);
-    
-       
-    
-        
-      useEffect(() => {
+    }, [InputValue]);
+
+
+
+
+    useEffect(() => {
         const timer = setTimeout(() => {
-          setInputValue(formValues.username.trim());
+            setInputValue(formValues.username.trim());
         }, 500);
-    
+
         return () => {
-          clearTimeout(timer);
+            clearTimeout(timer);
         };
-      }, [formValues.username]);
+    }, [formValues.username]);
 
 
-    const location=useLocation();
-    const state=location.state || {};
-    const gauth=searchParams.get('fromgauth');
-    if(!state || Object.keys(state).length===0){
-        if(!gauth){
-            return <Navigate  to={"/"} replace />
+    const location = useLocation();
+    const state = location.state || {};
+    const gauth = searchParams.get('fromgauth');
+    if (!state || Object.keys(state).length === 0) {
+        if (!gauth) {
+            return <Navigate to={"/"} replace />
         }
-    }else{
-        if(!state?.fromregister){
-            return <Navigate  to={"/"} replace />
+    } else {
+        if (!state?.fromregister) {
+            return <Navigate to={"/"} replace />
         }
     }
-       
-    
 
 
 
 
-    const allset=!loading && unique && isnormal;
-    
-   
+
+
+    const allset = !loading && unique && isnormal;
+
+
     const handleNextStep = () => {
         setCurrentStep((prev) => prev + 1);
     };
@@ -143,52 +143,45 @@ const CompleteProfile = () => {
         setCurrentStep((prev) => prev - 1);
     };
 
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if(isFormSubmitting) return ;
-        try {
-            setisFormSubmitting(true);
-    
-            const data = await usePutHook('/complete-profile', {
-                username: formValues.username,
-                name: formValues.firstName + " " + formValues.lastName,
-                college: formValues.college,
-                lc: formValues.leetcode,
-                cf: formValues.codeforces,
-                cc: formValues.codechef,
-                avatar: avatar,
-            }, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                }
-            });
-    
-            if (data.data && data.data.user) {
-                toast.success(`welcome to Codaez, ${data.data.user.name?.trim()?.length > 0 ? data.data.user.name : (data.data.user.username)}!`, {
-                    position: "top-right"
-                });
-                dispatch(setAuth(data.data.user));
-                navigate('/dashboard');
+        if (isFormSubmitting) return;
+        setisFormSubmitting(true);
+
+        const data = await usePutHook('/complete-profile', {
+            username: formValues.username,
+            name: formValues.firstName + " " + formValues.lastName,
+            college: formValues.college,
+            lc: formValues.leetcode,
+            cf: formValues.codeforces,
+            cc: formValues.codechef,
+            avatar: avatar,
+        }, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
             }
-            else if (data.data) {
-                toast.warn(data.data.error || data.data.message, {
-                    position: "top-right"
-                });
-            }
-            else {
-                console.log(data);
-                toast.error(data.error, {
-                    position: "top-right"
-                });
-            }
-        } catch (error) {
-            toast.error(error.message || "something went wrong!!!", {
+        });
+
+        if (data.data && data.data.user) {
+            toast.success(`welcome to Codaez, ${data.data.user.name?.trim()?.length > 0 ? data.data.user.name : (data.data.user.username)}!`, {
                 position: "top-right"
             });
-        }finally{
-            setisFormSubmitting(false);
+            dispatch(setAuth(data.data.user));
+            navigate('/dashboard');
         }
+        else if (data.data) {
+            toast.warn(data.data.error || data.data.message, {
+                position: "top-right"
+            });
+        }
+        else {
+            console.log(data);
+            toast.error(data.error, {
+                position: "top-right"
+            });
+        }
+        setisFormSubmitting(false);
     };
 
     const handleProfilePictureChange = (e) => {
@@ -215,12 +208,12 @@ const CompleteProfile = () => {
             [name]: value
         }));
         setLoading(true);
-        if(formValues.username.trim()===value.trim()){
-          setLoading(false);
+        if (formValues.username.trim() === value.trim()) {
+            setLoading(false);
         }
     };
-    
-   
+
+
 
     return (
         <div className="register-container">
@@ -242,11 +235,11 @@ const CompleteProfile = () => {
                         <div className={`progress-step ${currentStep > 3 ? 'completed' : currentStep == 3 ? 'active' : ''}`}>
                             {currentStep > 3 ? <FaCheck /> : '3'}
                         </div>
-                        <p>Coding Usenames</p>
+                        <p>Coding Usernames</p>
                     </div>
                 </div>
                 <div className="register-form">
-                    <form  onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit}>
                         {currentStep === 1 && (
                             <div className="register-step">
                                 <h3>User Credentails</h3>
@@ -259,17 +252,17 @@ const CompleteProfile = () => {
                                     {(formValues.username.length > 0) && (
                                         <div className="spinner">
                                             {loading ? (
-                                            <ClipLoader
-                                                loading={loading}
-                                                color="#814fff"
-                                                className="icon"
-                                                size={18}
-                                                speedMultiplier={1}
-                                            />
+                                                <ClipLoader
+                                                    loading={loading}
+                                                    color="#814fff"
+                                                    className="icon"
+                                                    size={18}
+                                                    speedMultiplier={1}
+                                                />
                                             ) : unique ? (
-                                            <SiTicktick size={18} color="#814fff" />
+                                                <SiTicktick size={18} color="#814fff" />
                                             ) : (
-                                            <RiErrorWarningLine size={18} color="#e9bc28" />
+                                                <RiErrorWarningLine size={18} color="#e9bc28" />
                                             )}
                                         </div>
                                     )}
@@ -293,7 +286,7 @@ const CompleteProfile = () => {
                                     </div>
                                 </div> */}
                                 <div className="register-buttons">
-                                    <button disabled={!allset} type="button" className={`${allset ? " " :"disable " } register-button`}onClick={handleNextStep}>
+                                    <button disabled={!allset} type="button" className={`${allset ? " " : "disable "} register-button`} onClick={handleNextStep}>
                                         Next
                                     </button>
                                 </div>
@@ -305,11 +298,12 @@ const CompleteProfile = () => {
                                 <div className="profile-input">
                                     <div className="profile-picture">
                                         {profilePicture ? (
-                                            <img  onClick={() =>{ setProfilePicture(null)
+                                            <img onClick={() => {
+                                                setProfilePicture(null)
                                                 avatarref.current.click()
-                                             }} src={profilePicture} alt="profile image" />
+                                            }} src={profilePicture} alt="profile image" />
                                         ) : (
-                                           <div onClick={() => avatarref.current.click()} ></div>
+                                            <div onClick={() => avatarref.current.click()} ></div>
                                         )}
                                         <input type="file" id="photo" ref={avatarref} name="photo" accept="image/*" onChange={(e) => handleProfilePictureChange(e)} />
                                     </div>
@@ -331,10 +325,12 @@ const CompleteProfile = () => {
                                 <div className="register-buttons">
 
                                     <button type="button" className="register-button" onClick={handlePrevStep}>
+                                        <IoIosArrowBack />
                                         Back
                                     </button>
                                     <button type="button" className="register-button" onClick={handleNextStep}>
                                         Next
+                                        <IoIosArrowForward />
                                     </button>
                                 </div>
                             </div>
@@ -371,10 +367,19 @@ const CompleteProfile = () => {
                                 </div>
                                 <div className='register-buttons'>
                                     <button type="button" className="register-button" onClick={handlePrevStep}>
+                                        <IoIosArrowBack />
                                         Back
                                     </button>
                                     <button disabled={isFormSubmitting} type="submit" className="register-button">
-                                      {isFormSubmitting ? "submitting....." :  "Submit"}
+                                        {isFormSubmitting
+                                            ?
+                                            <><ClipLoader
+                                                color="#ffffff"
+                                                className="icon"
+                                                size={18}
+                                                speedMultiplier={1}
+                                            /></>
+                                            : "Submit"}
                                     </button>
                                 </div>
                             </div>
