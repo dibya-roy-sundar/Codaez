32fa2c00a185de9cf0f6c40c69c6eac063b13f08
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-07-24 01:50:55 +0530

    refresh data and contest every 1-2 hrs

--- server/routes/details.js
+++ server/routes/details.js
@@ -2,12 +2,18 @@ const express = require('express');
 const { getUserDetails, getLeaderboardDetails } = require('../controllers/details');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn } = require('../utils/isLoggedIn');
-const { refreshData } = require('../RefreshData');
+const { refreshData, refreshUpContests } = require('../RefreshData');
 const router = express.Router({ mergeParams: true });
 
 router.route('/leaderboard').get(catchAsync(isLoggedIn), catchAsync(getLeaderboardDetails))
-router.route('/refreshdata').get((req, res, next) => {
-    const data = refreshData();
+router.route('/refreshuserdata').get(async(req, res, next) => {
+    const data = await refreshData();
+    return res.status(data.status || 400).json({
+        ...data
+    });
+});
+router.route('/refreshcontests').get(async(req, res, next) => {
+    const data = await refreshUpContests();
     return res.status(data.status || 400).json({
         ...data
     });
