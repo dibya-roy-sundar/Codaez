8ab6d2fef92c0adb6c28969e9fffa1ad42e18194
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-07-24 17:51:24 +0530

    aggregate formula update

--- server/utils/calcAggregateRating.js
+++ server/utils/calcAggregateRating.js
@@ -1,28 +1,25 @@
 const calcAggregateRating = (user) => {
     let n = 0, sum = 0, aggregateRating = 0;
 
-    min_ratings = { lc: 1200, cf: 800, cc: 800 }
-    max_ratings = { lc: 3000, cf: 2700, cc: 2700 }
+    min_ratings = { lc: 1000, cf: 800, cc: 800 }
+    max_ratings = { lc: 3200, cf: 3000, cc: 3000 }
 
     if (user?.lc?.rating && user.lc.rating>0) {
         sum += (user.lc.rating - min_ratings.lc) / (max_ratings.lc - min_ratings.lc)
         n++;
-        console.log('lc')
     }
     if (user?.cf?.rating && user.cf.rating>0) {
         sum += (user.cf.rating - min_ratings.cf) / (max_ratings.cf - min_ratings.cf)
         n++;
-        console.log('cf')
     }
     if (user?.cc?.rating && user.cc.rating>0) {
         sum += (user.cc.rating - min_ratings.cc) / (max_ratings.cc - min_ratings.cc)
         n++;
-        console.log('cc')
     }
     if (n > 0) {
-        aggregateRating = Math.round(sum / n * 10000);
+        aggregateRating = Math.round(sum / n * 5000);
     }
-    console.log("ag",sum,n,aggregateRating)
+    console.log("aggregate",sum,n,aggregateRating)
 
     return aggregateRating
 }
