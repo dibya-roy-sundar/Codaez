b0646b8d25e4576952fc352c4e9fde259f9e885e
Dibya Sunadr Roy <dibya.roy.12345@gmail.com>
2024-07-25 22:59:28 +0530

    add isloggedin in search

--- server/routes/user.js
+++ server/routes/user.js
@@ -61,7 +61,7 @@ router.route('/setusername').post(catchAsync(isLoggedIn), catchAsync(setUsername
 router.route('/logout').get(catchAsync(logout));
 
 
-router.route('/userdetails').get(catchAsync(userDetails));//search catchAsync(isLoggedIn),
+router.route('/userdetails').get(catchAsync(isLoggedIn),catchAsync(userDetails));//search 
 
 router.route('/get-requests').get(catchAsync(isLoggedIn), catchAsync(getReqeusts));//
 router.route('/sendfrequest').post(catchAsync(isLoggedIn), catchAsync(sendFollowRequest));
