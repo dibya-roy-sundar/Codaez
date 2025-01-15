e6da9a49b045d6c9b55b77d82f737d53be16a744
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2025-01-16 00:43:06 +0530

    trying PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Codaez',
+        short_name: 'Codaez',
+        description: 'Analyze, Improve, Dominate Together',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/favicon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          // {
+          //   src: '/icons/icon-512x512.png',
+          //   sizes: '512x512',
+          //   type: 'image/png',
+          // },
+        ],
+        orientation: 'portrait'
+      },
+    }),
+  ],
 })
